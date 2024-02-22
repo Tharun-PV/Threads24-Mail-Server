35205e5ec93914edaefd9729b717972aeed239c5
Tharun-PV <rolyxtharun1@gmail.com>
2024-02-22 09:39:52 +0530

    11

--- backend/Server.js
+++ backend/Server.js
@@ -1,19 +1,31 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors');  // Add this line
 const MongoClient = require('mongodb').MongoClient;
 const ExcelJS = require('exceljs');
 const fs = require('fs');
 const nodemailer = require('nodemailer');
 const { promisify } = require('util');
 const cron = require('node-cron');
-
 const app = express();
-const port = process.env.PORT || 4000; // Use the provided port or default to 4000
+const port = 4000;
 
-app.use(cors());
+app.use(cors())
 
-const mongoUri = process.env.MONGO_URI; // Use environment variable for MongoDB URI
-const databaseName = process.env.DATABASE_NAME || "test"; // Use environment variable for database name or default to "test"
+
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+app.use(cors({
+  origin: 'https:download.sonacse.in',
+}));
+
+
+const mongoUri = "mongodb+srv://sona123:sona123@threadsapp.scz5gdt.mongodb.net/?retryWrites=true&w=majority";
+const databaseName = "test";
 
 async function exportCollectionsToCSV() {
   const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -55,10 +67,11 @@ async function exportCollectionsToCSV() {
   }
 }
 
+
 app.get('/download-excel', async (req, res) => {
   try {
     const filePath = await exportCollectionsToCSV();
-
+    
     // Send the file to the client
     res.download(filePath, 'output.xlsx', (err) => {
       if (err) {
@@ -73,6 +86,7 @@ app.get('/download-excel', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
 
 app.get('/collections', async (req, res) => {
   const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -92,6 +106,7 @@ app.get('/collections', async (req, res) => {
     await client.close();
   }
 });
+
 
 app.get('/collection-data', async (req, res) => {
   const collectionName = req.query.name;
@@ -122,6 +137,7 @@ const gmailCredentials = {
   password: 'sabzvrimgumxfihr',
 };
 
+
 const sendEmail = async () => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -133,9 +149,9 @@ const sendEmail = async () => {
 
   const mailOptions = {
     from: gmailCredentials.email,
-    to: 'threads.cse@sonatech.ac.in',
+    to: 'threads.cse@sonatech.ac.in', 
     subject: 'CSV Export',
-    text: 'Vanakam Da Mapla, This is the attachment of the database for every 5 Mins.',
+    text: 'Vanakam Da Mapla, This is the attachment of database for every 5 Mins.',
     attachments: [
       {
         filename: 'output.xlsx',
@@ -154,12 +170,16 @@ const sendEmail = async () => {
   }
 };
 
+
 cron.schedule('*/5 * * * *', async () => {
   console.log('Exporting data and sending email...');
 
   try {
+
     const filePath = await exportCollectionsToCSV();
+
     await sendEmail();
+
     fs.unlinkSync(filePath);
   } catch (error) {
     console.error('Error in cron job:', error);
